a2416954617b5cef4426cea57d1967f01d357ef9
AllenZhong <2642754767@qq.com>
2016-07-12 10:00:15 +0800

    登录和发送消息举例

--- netty-chat-client/src/main/java/net/datafans/netty/chat/client/SimpleClient.java
+++ netty-chat-client/src/main/java/net/datafans/netty/chat/client/SimpleClient.java
@@ -1,19 +1,33 @@
 package net.datafans.netty.chat.client;
 
+import com.alibaba.fastjson.JSON;
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import io.netty.channel.ChannelHandler;
 
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import net.datafans.netty.chat.client.handler.DataPackageHandler;
+import net.datafans.netty.chat.common.constant.BizType;
 import net.datafans.netty.chat.common.constant.Protocal;
+import net.datafans.netty.chat.common.constant.Version;
 import net.datafans.netty.chat.common.entity.DataPackage;
+import net.datafans.netty.chat.common.entity.response.SuccessResponse;
 import net.datafans.netty.chat.common.handler.DataPackageDecoder;
 import net.datafans.netty.chat.common.handler.DataPackageEncoder;
 import net.datafans.netty.common.client.NettyClient;
 import net.datafans.netty.common.config.GlobalConfig;
 import net.datafans.netty.common.handler.ChannelHandlerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SimpleClient extends NettyClient {
+
+
+    private static  Logger logger = LoggerFactory.getLogger(SimpleClient.class);
 
     @Override
     protected boolean enableFrameDecoder() {
@@ -76,6 +90,35 @@ public class SimpleClient extends NettyClient {
     public static void main(String[] args) {
 
         final SimpleClient client = new SimpleClient();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                logger.info("开始登录");
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("user_id", 100);
+                DataPackage login = new DataPackage(Version.V1, BizType.LOGIN, JSON
+                        .toJSONString(map).getBytes());
+                client.write(login);
+                logger.info("结束登录");
+
+                logger.info("发送普通消息");
+                Map<String, Object> simpleMsgMap = new HashMap<String, Object>();
+                map.put("text", "hello world!");
+                DataPackage simpleMsgPkt = new DataPackage(Version.V1, BizType.SIMPLE_MSG, JSON
+                        .toJSONString(simpleMsgMap).getBytes());
+                client.write(simpleMsgPkt);
+                logger.info("发送普通消息完成");
+
+
+            }
+        }).start();
         client.start();
     }
 
